63214a714d79720bc523b11348aa1dd8525ef6a5
EC2 Default User <ec2-user@ip-172-31-41-8.ec2.internal>
2019-09-23 18:28:49 +0000

    second commit

--- docker.sh
+++ docker.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 docker pull elasticsearch:7.3.2
 docker run -itd --name elasticsearch -p 9200:9200 -p 9300:9300 --health-cmd "curl --fail http://localhost:9200 || exit 1"  -e "discovery.type=single-node" elasticsearch:7.3.2
 docker inspect --format='{{ .State.Health}}' elasticsearch
